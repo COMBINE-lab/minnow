99388f62cd86c76d5016d9a0eb593b7e93219c5c
hiraksarkar <hsarkar@cs.stonybrook.edu>
2019-09-25 14:00:52 -0400

    staging alevin mode for refactor

---
+++ include/LibraryConfig.hpp
@@ -0,0 +1,67 @@
+#ifndef __LIBRARY_CONFIG_HPP__
+#define __LIBRARY_CONFIG_HPP__
+
+#include <string>
+#include <cmath>
+
+#include "ProgOpts.hpp"
+
+namespace protocol{
+
+  inline uint32_t calculatePoolSize(uint32_t umiLength){
+    return std::pow(4, umiLength) ;
+  }
+
+  class SingleCellProtocolConfig{
+  public:
+    SingleCellProtocolConfig(){}
+
+    SingleCellProtocolConfig(
+        uint32_t barcodeLength_,
+        uint32_t umiLength_,
+        BarcodeEnd end_):
+      barcodeLength(barcodeLength_),
+      umiLength(umiLength_),
+      end(end_){
+      maxValue = calculatePoolSize(umiLength) ;
+      protocoltype = ProtocolType::CUSTOM ;
+    }
+
+    SingleCellProtocolConfig(
+                              uint32_t barcodeLength_,
+                              uint32_t umiLength_,
+                              BarcodeEnd end_,
+                              uint32_t maxValue_,
+                              ProtocolType protocoltype_
+                              ):
+      barcodeLength(barcodeLength_),
+      umiLength(umiLength_),
+      end(end_),
+      maxValue(maxValue_),
+      protocoltype(protocoltype_){}
+
+    uint32_t barcodeLength ;
+    uint32_t umiLength ;
+    BarcodeEnd end ;
+    uint32_t maxValue ;
+    ProtocolType protocoltype ;
+  };
+
+  inline SingleCellProtocolConfig constructProtocol(std::string& protocolStr){
+    if(protocolStr == "Chromiumv3"){
+      return SingleCellProtocolConfig(16,12,BarcodeEnd::FIVE,4294967295,ProtocolType::CHROMIUMV3) ;
+    }else if(protocolStr == "Chromium"){
+        return SingleCellProtocolConfig(16,10,BarcodeEnd::FIVE,4294967295,ProtocolType::CHROMIUM) ;
+    }else if(protocolStr == "DropSeq"){
+      return SingleCellProtocolConfig(12,8,BarcodeEnd::FIVE,16777216,ProtocolType::DROPSEQ) ;
+    }else if(protocolStr == "CelSeq"){
+      return SingleCellProtocolConfig(8,6,BarcodeEnd::FIVE,16777216,ProtocolType::CELSEQ) ;
+    }else if(protocolStr == "CelSeq2"){
+      return SingleCellProtocolConfig(6,6,BarcodeEnd::FIVE,16777216,ProtocolType::CELSEQ2) ;
+    }else{
+      return SingleCellProtocolConfig(16,12,BarcodeEnd::FIVE,4294967295,ProtocolType::CHROMIUMV3) ;
+    }
+  }
+
+}
+#endif
